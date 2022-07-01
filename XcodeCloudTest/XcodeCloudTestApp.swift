9bd866ebb4e67404096dddc38ba64ffd528e1c3b
Magnus Romnes <magnus.romnes@shortcut.no>
2022-07-01 07:19:28 +0200

    Initial Commit

---
+++ XcodeCloudTest/XcodeCloudTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeCloudTestApp.swift
+//  XcodeCloudTest
+//
+//  Created by Magnus Romnes on 01/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct XcodeCloudTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
